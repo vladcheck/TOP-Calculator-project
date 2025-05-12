2b1809b14ae1206a84f332c93140650c99d8d579
Vladimir Valekzhanin <valdemarcheck@yandex.ru>
2025-05-12 20:37:07 +0300

    Setup Calculate module

---
+++ index.js
@@ -0,0 +1,7 @@
+const Calculate = (() => {
+  const add = (x, y) => x + y;
+  const subtract = (x, y) => x - y;
+  const multiply = (x, y) => x * y;
+  const divide = (x, y) => x / y;
+  return { add, subtract, multiply, divide };
+})();
